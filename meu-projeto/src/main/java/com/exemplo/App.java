30ac1180a586b88e0aa385fccd93b52c8afd80c6
wesleypds <wlypereiraa@gmail.com>
2025-01-31 13:47:58 -0300

    configurando projeto

--- meu-projeto/src/main/java/com/exemplo/App.java
+++ meu-projeto/src/main/java/com/exemplo/App.java
@@ -1,13 +1,17 @@
 package com.exemplo;
 
-/**
- * Hello world!
- *
- */
+import com.exemplo.dominio.Pessoa;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Pessoa p1 = new Pessoa(1, "Camila Pereira", "camila@gmail.com");
+        Pessoa p2 = new Pessoa(2, "Wesley Pereira", "wesley@gmail.com");
+        Pessoa p3 = new Pessoa(3, "Neusa Pereira", "neusa@gmail.com");
+
+        System.err.println(p1);
+        System.err.println(p2);
+        System.err.println(p3);
     }
 }
